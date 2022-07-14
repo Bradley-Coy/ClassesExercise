957563dc59798be087359779d0e13076ff43495f
Bradley-Coy <bradleycoy95@gmail.com>
2022-07-14 09:28:15 -0700

    first commit

---
+++ ClassesExercise/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace ClassesExercise
+{
+	public class Car
+	{
+
+		public string Make { get; set; }
+		public string Model { get; set; }
+		public int Year { get; set; }
+
+
+
+
+
+
+
+		/*public int Year;
+		public string Make;
+		public string Model;
+		*/
+        //public Car(string make, string model, int year)
+        //{
+			//Car car1 = new Car("Ford", "Taurus", 2002);
+		//}
+
+        /*/internal Car(string make, string model, int year)
+        //{
+			Make = make;
+			Model = model;
+			Year = year;
+			
+        }*/
+    }
+	
+}
+
---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using ClassesExercise;
+
+
+//car1.Year = 2002;
+Car car1 = new ()
+{
+    Make = "Ford",
+    Model = "Focus",
+    Year = 2005
+};
+
+Console.WriteLine(car1);
+
+
+//var car2 = new Car() {M\ake = "Honda", Model = "Civic", Year = 2006 };
